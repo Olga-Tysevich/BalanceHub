ba3b732d8c1c22bca651c154ab5b031be6ad3cf0
Olga <olga.tisevich@gmail.com>
2025-05-09 15:26:43 +0300

    Добавлены дто и мапперы для основных сущностей

---
+++ src/main/java/by/testtask/balancehub/dto/UserDTO.java
@@ -0,0 +1,103 @@
+package by.testtask.balancehub.dto;
+
+import by.testtask.balancehub.domain.EmailData;
+import by.testtask.balancehub.domain.PhoneData;
+import by.testtask.balancehub.utils.validators.CollectionSize;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static by.testtask.balancehub.utils.Constants.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDTO {
+
+    @NotNull(message = ID_CANNOT_BE_NULL)
+    private Long id;
+
+    @NotBlank(message = NAME_CANNOT_BE_EMPTY)
+    @Size(max = 500, message = NAME_CANNOT_BE_GZ_500)
+    private String name;
+
+    @Past(message = DATE_OF_BIRTHDAY_MUST_BE_IN_PAST)
+    @JsonFormat(pattern = DATE_OF_BIRTHDAY_PATTERN)
+    private LocalDate dateOfBirthday;
+
+    @CollectionSize(message = EMPTY_PHONE_SET)
+    private Set<InnerPhoneData> phones;
+
+    @CollectionSize(message = EMPTY_PHONE_SET)
+    private Set<InnerEmailData> emails;
+
+    public void setPhoneFromDTO(Set<PhoneDataDTO> phones) {
+        this.phones = phones.stream()
+                .map(InnerPhoneData::new)
+                .collect(Collectors.toSet());
+    }
+
+    public void setEmailFromDTO(Set<EmailDataDTO> emails) {
+        this.emails = emails.stream()
+                .map(InnerEmailData::new)
+                .collect(Collectors.toSet());
+    }
+
+    public void setPhoneFrom(Set<PhoneData> phones) {
+        this.phones = phones.stream()
+                .map(InnerPhoneData::new)
+                .collect(Collectors.toSet());
+    }
+
+    public void innerEmailFrom(Set<EmailData> emails) {
+        this.emails = emails.stream()
+                .map(InnerEmailData::new)
+                .collect(Collectors.toSet());
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class InnerPhoneData {
+        private Long id;
+        private String phone;
+
+        public InnerPhoneData(PhoneDataDTO phone) {
+            this.id = phone.getId();
+            this.phone = phone.getPhoneNumber();
+        }
+
+        public InnerPhoneData(PhoneData phone) {
+            this.id = phone.getId();
+            this.phone = phone.getPhoneNumber();
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class InnerEmailData {
+        private Long id;
+        private String email;
+
+        public InnerEmailData(EmailDataDTO email) {
+            this.id = email.getId();
+            this.email = email.getEmail();
+        }
+
+        public InnerEmailData(EmailData email) {
+            this.id = email.getId();
+            this.email = email.getEmail();
+        }
+    }
+
+}
---
+++ src/main/java/by/testtask/balancehub/mappers/AccountMapper.java
@@ -0,0 +1,34 @@
+package by.testtask.balancehub.mappers;
+
+import by.testtask.balancehub.domain.Account;
+import by.testtask.balancehub.domain.User;
+import by.testtask.balancehub.dto.AccountDTO;
+import org.mapstruct.*;
+
+import java.util.Collection;
+
+@Mapper(
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+)
+public interface AccountMapper {
+    @Mappings({
+            @Mapping(target = "user", ignore = true)
+    })
+    Account toEntity(final AccountDTO accountDTO);
+
+    @Mappings({
+            @Mapping(target = "id", source = "accountDTO.id"),
+            @Mapping(target = "user", source = "owner")
+    })
+    Account toEntity(final AccountDTO accountDTO, final User owner);
+
+    @Mappings({
+            @Mapping(target = "userId", source = "user.id")
+    })
+    AccountDTO toDTO(final Account account);
+
+    Collection<Account> allToEntities(final Collection<AccountDTO> accountDTOs);
+
+    Collection<AccountDTO> allToDTOs(final Collection<Account> accounts);
+}
