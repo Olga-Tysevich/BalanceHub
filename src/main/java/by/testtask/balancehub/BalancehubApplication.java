a4362c299d9c81b9eb4f1b221df954d0df338d70
Olga <olga.tisevich@gmail.com>
2025-05-09 12:48:43 +0300

    initial

---
+++ src/main/java/by/testtask/balancehub/BalancehubApplication.java
@@ -0,0 +1,13 @@
+package by.testtask.balancehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalancehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalancehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/testtask/balancehub/BalancehubApplicationTests.java
@@ -0,0 +1,13 @@
+package by.testtask.balancehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalancehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
