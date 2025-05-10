5ae24d6d402dddd1d6a11d99be7cb8e53f3a179b
Olga <olga.tisevich@gmail.com>
2025-05-10 17:34:51 +0300

    Добавлен докер-клнтейнер для тестовой среды для эластика

--- src/test/java/by/testtask/balancehub/BaseTest.java
+++ src/test/java/by/testtask/balancehub/BaseTest.java
@@ -5,21 +5,30 @@ import by.testtask.balancehub.utils.PostgresSQL;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ContextConfiguration(initializers = PostgresSQL.Initializer.class)
 @TestPropertySource(locations = "classpath:application-test.properties")
 public abstract class BaseTest {
-
     @BeforeAll
-    static void initTestContainer() {
+    static void initContainers() {
         PostgresSQL.container.start();
         Elasticsearch.container.start();
+    }
+
+    @DynamicPropertySource
+    static void registerDynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", PostgresSQL.container::getJdbcUrl);
+        registry.add("spring.datasource.username", PostgresSQL.container::getUsername);
+        registry.add("spring.datasource.password", PostgresSQL.container::getPassword);
+        registry.add("spring.data.elasticsearch.uris", Elasticsearch::getElasticsearchUrl);
+        registry.add("spring.data.elasticsearch.username", () -> "elastic");
+        registry.add("spring.data.elasticsearch.password", () -> "password");
     }
 
 }
--- src/test/java/by/testtask/balancehub/utils/Elasticsearch.java
+++ src/test/java/by/testtask/balancehub/utils/Elasticsearch.java
@@ -1,41 +1,33 @@
 package by.testtask.balancehub.utils;
 
 import lombok.experimental.UtilityClass;
-import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
+
+import java.time.Duration;
 
 @UtilityClass
 public class Elasticsearch {
 
     public static final GenericContainer<?> container =
-            new GenericContainer<>(DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch:latest"))
-            .withExposedPorts(9200)
-            .withEnv("discovery.type", "single-node");
+            new GenericContainer<>(DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch:8.6.0"))
+                    .withExposedPorts(9200)
+                    .withEnv("discovery.type", "single-node")
+                    .withEnv("ELASTIC_USERNAME", "elastic")
+                    .withEnv("ELASTIC_PASSWORD", "password")
+                    .withEnv("ES_JAVA_OPTS", "-Xms2g -Xmx2g")
+                    .withEnv("xpack.security.enabled", "false")
+                    .withEnv("xpack.security.http.ssl.enabled", "false")
+                    .waitingFor(Wait.forHttp("/").forStatusCode(200).withStartupTimeout(Duration.ofMinutes(5)));
+
 
     static {
         container.start();
+        System.out.println(container.getLogs());
     }
 
-    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-
-        @Override
-        public void initialize(ConfigurableApplicationContext applicationContext) {
-            TestPropertyValues.of(
-                    "spring.elasticsearch.rest.uris=" + getElasticsearchUrl()
-            ).applyTo(applicationContext);
-        }
-    }
-
-    // Метод для получения URL Elasticsearch
     public static String getElasticsearchUrl() {
         return "http://" + container.getHost() + ":" + container.getMappedPort(9200);
-    }
-
-    // Метод для остановки контейнера (если необходимо)
-    public static void stop() {
-        container.stop();
     }
 }
