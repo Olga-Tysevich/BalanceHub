9f276d7994be38eb7c33e0427c72b0e3a04c84a9
Olga <olga.tisevich@gmail.com>
2025-05-12 01:18:29 +0300

    Добавила тест для трансфера

--- src/test/java/by/testtask/balancehub/services/impl/AccountServiceImplTest.java
+++ src/test/java/by/testtask/balancehub/services/impl/AccountServiceImplTest.java
@@ -4,12 +4,10 @@ import by.testtask.balancehub.BaseTest;
 import by.testtask.balancehub.domain.Account;
 import by.testtask.balancehub.domain.Transfer;
 import by.testtask.balancehub.dto.req.MoneyTransferReq;
-import by.testtask.balancehub.events.listeners.TransferEventListener;
 import by.testtask.balancehub.repos.AccountRepo;
 import by.testtask.balancehub.repos.TransferRepo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -18,8 +16,6 @@ import java.math.BigDecimal;
 
 import static by.testtask.balancehub.utils.TestConstants.USERNAME_1_EMAIL_LIST;
 import static by.testtask.balancehub.utils.TestConstants.USERNAME_1_PASSWORD;
-import static org.mockito.Mockito.*;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith({SpringExtension.class, MockitoExtension.class})
@@ -52,6 +48,7 @@ class AccountServiceIntegrationTest extends BaseTest {
         transfer.setToAccountId(toAccount.getId());
         transfer.setAmount(new BigDecimal("50.00"));
 
+        super.setAuthentication(USERNAME_1_EMAIL_LIST.getFirst(), USERNAME_1_PASSWORD);
         accountService.createTransfer(transfer);
 
         Transfer processedTransfer = new Transfer();
