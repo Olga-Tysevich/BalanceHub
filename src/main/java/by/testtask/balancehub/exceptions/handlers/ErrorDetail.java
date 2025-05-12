9aacd4f4f276d3b3a15a3bf9b1f1094fe72fddef
Olga <olga.tisevich@gmail.com>
2025-05-12 15:43:31 +0300

    Добавила логги

--- src/main/java/by/testtask/balancehub/exceptions/handlers/ErrorDetail.java
+++ src/main/java/by/testtask/balancehub/exceptions/handlers/ErrorDetail.java
@@ -1,12 +1,5 @@
 package by.testtask.balancehub.exceptions.handlers;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
-public class ErrorDetail {
-
-    private final String fieldName;
-    private final String message;
+public record ErrorDetail(String fieldName, String message) {
 
 }
