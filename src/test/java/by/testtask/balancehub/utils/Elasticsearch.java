57e330036021ad9dc0b0629811a365b000bd4184
Olga <olga.tisevich@gmail.com>
2025-05-12 16:13:29 +0300

    Добавила свагер

--- src/test/java/by/testtask/balancehub/utils/Elasticsearch.java
+++ src/test/java/by/testtask/balancehub/utils/Elasticsearch.java
@@ -24,7 +24,6 @@ public class Elasticsearch {
 
     static {
         container.start();
-        System.out.println(container.getLogs());
     }
 
     public static String getElasticsearchUrl() {
